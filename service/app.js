3fbbb85bd933f8807965f42f57373898b75fdbf8
DiamondsZz <DiamondsZzZz@163.com>
2021-01-23 18:01:21 +0800

    test-ejs

--- service/app.js
+++ service/app.js
@@ -4,17 +4,32 @@ const Views = require("koa-views");
 const Router = require("koa-router");
 
 const App = new Koa();
-const Route=new Router()
+const Route = new Router();
 
 App.use(Views(__dirname, { extension: "ejs" }));
-App.use(Route.routes())
-App.use(Route.allowedMethods())
-
+App.use(Route.routes());
+App.use(Route.allowedMethods());
 
 App.listen(3000);
 
 Route.get("/", async (context) => {
   await context.render("index", {
-    title: "hello world",
+    data: [
+      //文本
+      {
+        type: "text",
+        value: "hello world",
+        style: {
+          fontSize: "16px",
+        },
+      },
+      //图片
+      {
+        type: "img",
+        value:
+          "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+        style: { width: 40 },
+      },
+    ],
   });
 });

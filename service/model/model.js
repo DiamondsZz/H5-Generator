fa90e3b98af6f578f478fe0d28c2fd3f825eec7d
DiamondsZz <DiamondsZzZz@163.com>
2021-01-23 16:38:34 +0800

    test-model

---
+++ service/model/model.js
@@ -0,0 +1,22 @@
+// 定义页面数据结构
+
+{
+  //页面数据
+  data: [
+    //文本
+    {
+      type: "text",
+      value: "hello world",
+      style: {
+        fontSize: "16px",
+      },
+    },
+    //图片
+    {
+      type: "img",
+      value:
+        "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+      style: {},
+    },
+  ];
+}

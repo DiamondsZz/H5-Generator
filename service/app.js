37d47152864c59553967882f87b1103b918129a0
DiamondsZz <DiamondsZzZz@163.com>
2021-01-23 15:40:55 +0800

    koa-views/koa-router/ejs

--- service/app.js
+++ service/app.js
@@ -1,7 +1,20 @@
 const Koa = require("koa");
+
+const Views = require("koa-views");
+const Router = require("koa-router");
+
 const App = new Koa();
-App.use(async (context) => {
-  context.body = "hello world";
-});
+const Route=new Router()
+
+App.use(Views(__dirname, { extension: "ejs" }));
+App.use(Route.routes())
+App.use(Route.allowedMethods())
+
 
 App.listen(3000);
+
+Route.get("/", async (context) => {
+  await context.render("index", {
+    title: "hello world",
+  });
+});

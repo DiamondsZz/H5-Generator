f08ed2b0cc244a2bf07b45f917c6c2224d66d28c
DiamondsZz <DiamondsZzZz@163.com>
2021-02-02 15:10:12 +0800

    test

--- service/app.js
+++ service/app.js
@@ -1,3 +1,5 @@
+const Path = require("path");
+
 const Koa = require("koa");
 
 const Views = require("koa-views");
@@ -6,30 +8,45 @@ const Router = require("koa-router");
 const App = new Koa();
 const Route = new Router();
 
-App.use(Views(__dirname, { extension: "ejs" }));
+App.use(Views(Path.join(__dirname, "ejs"), { extension: "ejs" }));
 App.use(Route.routes());
 App.use(Route.allowedMethods());
 
 App.listen(3000);
 
+const styles = [
+  {
+    id: "text",
+    tag: "span",
+    style: {
+      "font-size": "16px",
+    },
+  },
+  {
+    id: "img",
+    tag: "img",
+    style: { width: "40px" },
+  },
+];
+
+const data = [
+  //文本
+  {
+    id: "text",
+    type: "text",
+    value: "hello world",
+  },
+  //图片
+  {
+    id: "img",
+    type: "img",
+    value:
+      "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+  },
+];
+
 Route.get("/", async (context) => {
   await context.render("index", {
-    data: [
-      //文本
-      {
-        type: "text",
-        value: "hello world",
-        style: {
-          fontSize: "16px",
-        },
-      },
-      //图片
-      {
-        type: "img",
-        value:
-          "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
-        style: { width: 40 },
-      },
-    ],
+    data,
   });
 });

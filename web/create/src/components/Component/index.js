dd999ca80d60330b2db881cf14675d3a3d6ab095
DiamondsZz <DiamondsZzZz@163.com>
2021-02-03 14:43:35 +0800

    antd-react-init

--- web/create/src/components/Component/index.js
+++ web/create/src/components/Component/index.js
@@ -1,12 +1,20 @@
 import React from "react";
-
+import { Image,Rate  } from "antd";
 class Component extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    return <div>666</div>;
+    return (
+      <div>
+        <Rate/>
+        <Image
+          width={200}
+          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+        />
+      </div>
+    );
   }
 }
 

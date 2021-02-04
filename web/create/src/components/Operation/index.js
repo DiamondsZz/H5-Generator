827704dd1664d9ed999bfd67fee6542088d74ae0
DiamondsZz <DiamondsZzZz@163.com>
2021-02-04 16:25:47 +0800

    color-control

--- web/create/src/components/Operation/index.js
+++ web/create/src/components/Operation/index.js
@@ -1,15 +1,64 @@
 import React from "react";
-
+import { Drawer, Divider, Tag } from "antd";
+import { CompactPicker } from "react-color";
 import "./style.css";
+
 class Operation extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      //属性弹窗显示
+      visible: false,
+      //区域样式
+      style: {},
+    };
   }
-
+  //显示属性弹窗
+  showDrawer() {
+    this.setState({ visible: true });
+  } //隐藏属性弹窗
+  hideDrawer(e) {
+    this.setState({ visible: false });
+  }
+  //颜色改变
+  colorChange(color) {
+    console.log(color);
+    this.setState({
+      style: {
+        backgroundColor: color.hex,
+      },
+    });
+  }
   render() {
     return (
       <div className="operation">
-        <div className="operation-area"></div>
+        <div
+          className="operation-area"
+          style={this.state.style}
+          onClick={() => this.showDrawer()}
+        ></div>
+        <Drawer
+          title="组件属性"
+          width={400}
+          placement="right"
+          visible={this.state.visible}
+          mask={false}
+          onClose={(e) => this.hideDrawer(e)}
+        >
+          <Tag
+            color="magenta"
+            style={{
+              display: "block",
+              width: "60px",
+              textAlign: "center",
+              marginBottom: "10px",
+            }}
+          >
+            背景色
+          </Tag>
+          <CompactPicker onChange={(color) => this.colorChange(color)} />
+          <Divider />
+        </Drawer>
       </div>
     );
   }
--- web/create/src/index.js
+++ web/create/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import 'antd/dist/antd.css'
+import 'react-contexify/dist/ReactContexify.css';
 import App from "./components/App/";
 
 ReactDOM.render(
